cb38ca714fe16aea674aa51f026d6fc5ddceb3ee
Ana Batista <anajessica.oliveirabt@gmail.com>
2015-11-28 13:03:47 -0500

    first commit of code

---
+++ app/src/main/java/distracker/hackwestern/Distracker.java
@@ -0,0 +1,46 @@
+package distracker.hackwestern;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+
+public class Distracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distracker);
+
+       LoginButton facebookLogin = (LoginButton) findViewById(R.layout.fblogin_button);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
